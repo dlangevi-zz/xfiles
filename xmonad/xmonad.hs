4579a543bd030cb370b2d18403462e1687b35af1
David Langevin <dlangevi@fb.com>
2017-08-29 13:31:50 -0700

    updates

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -8,11 +8,15 @@ import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeysP)
 import System.IO
 
-import XMonad.Config.Gnome
+import XMonad.Config.Desktop
 import XMonad.Hooks.SetWMName
 
 import Data.Monoid
 import Data.Map as M
+
+import XMonad.Util.Run (safeSpawn)
+
+import System.Environment (getEnvironment)
 
 import qualified XMonad.StackSet as W
 
@@ -23,13 +27,13 @@ myManageHook = composeAll [
                 className =? "Vncviewer" --> doFloat,
                 isFullscreen             --> doFullFloat
                ]
-myKeys = 
+{--myKeys = 
         [ ("M-" ++ ws, windows $ W.greedyView ws) | ws <- myWorkspaces ]
         ++
         [ ("M-S-" ++ ws, windows $ W.shift ws) | ws <- myWorkspaces ]
         ++
         [ ("M-l", spawn "gnome-screensaver-command -l") ]
-{--        ++
+        ++
         [ ("M-c", spawn "chromium") ]
         ++
         [ ("M-v", spawn "chromium --incognito") ]
@@ -48,6 +52,11 @@ myKeys =
         ++
         [ ("M-<F10>", spawn "amixer set Master 5+ unmute") ] --}
 
+myKeys (XConfig {modMask = modm}) = M.fromList $
+    [ ((modm, xK_p), spawn "dmenu_run") ]
+
+
+
 toggleFullFloat = withFocused (\w -> do
                                  { floats <- gets (W.floating . windowset);
                                    if M.member w floats
@@ -59,21 +68,91 @@ myStartupHook :: X ()
 myStartupHook = do setWMName "LG3D" 
                    spawn "~/bin/autostart"
 
-main = do xmproc <- spawnPipe "xmobar"
-          xmonad $ defaultConfig 
-          -- xmonad $ gnomeConfig
-                  { terminal  = "~/bin/termInit"
-    		      , borderWidth = 2
+main = do xmproc <- spawnPipe "/usr/bin/xmobar /home/dlangevi/.xmobarrc"
+          xmonad $ docks $ defaultConfig 
+                  { manageHook  = myManageHook <+> manageHook defaultConfig
+                  , terminal = "urxvt"
                   , workspaces  = myWorkspaces
+                  , layoutHook  = smartBorders $ avoidStruts $ layoutHook defaultConfig 
+	          , borderWidth = 2
                   , normalBorderColor  = "#646464"
-                  , focusedBorderColor = "darkgreen"
-                  , startupHook = myStartupHook
-                  , manageHook  = manageDocks <+> myManageHook <+> manageHook defaultConfig
-                  , layoutHook  = smartBorders $ avoidStruts $ layoutHook defaultConfig
+                  , focusedBorderColor = "lightgreen"
                   , logHook     = dynamicLogWithPP $ xmobarPP 
                                                       { ppOutput = hPutStrLn xmproc
                                                       , ppCurrent = xmobarColor "#ABABAB" "" . wrap "[" "]"
                                                       , ppTitle = xmobarColor "darkgreen" "" . shorten 100
                                                       }
-                  } `additionalKeysP` myKeys
+		  , startupHook = gnomeRegister >> startupHook desktopConfig 
+                  }
+
+-- | Register xmonad with gnome. 'dbus-send' must be in the $PATH with which
+-- xmonad is started.
+--
+-- This action reduces a delay on startup only only if you have configured
+-- gnome-session>=2.26: to start xmonad with a command as such:
+--
+-- > gconftool-2 -s /desktop/gnome/session/required_components/windowmanager xmonad --type string
+gnomeRegister :: MonadIO m => m ()
+gnomeRegister = io $ do
+    x <- Prelude.lookup "DESKTOP_AUTOSTART_ID" `fmap` getEnvironment
+    spawn "~/bin/autostart"
+    whenJust x $ \sessionId -> safeSpawn "dbus-send"
+            ["--session"
+            ,"--print-reply=literal"
+            ,"--dest=org.gnome.SessionManager"
+            ,"/org/gnome/SessionManager"
+            ,"org.gnome.SessionManager.RegisterClient"
+            ,"string:xmonad"
+            ,"string:"++sessionId]
+
+{--themain = do xmproc <- spawnPipe "xmobar"
+          --xmonad $ defaultConfig 
+          xmonad $ defaultConfig
+                  --{ terminal  = "~/bin/termInit"
+                  { terminal  = "xterm"
+    		      , borderWidth = 2
+                  , workspaces  = myWorkspaces
+                  --, startupHook = myStartupHook
+		  , XMonad.keys = myKeys <+> XMonad.keys desktopConfig
+                  , logHook     = dynamicLogWithPP $ xmobarPP 
+                                                      { ppOutput = hPutStrLn xmproc
+                                                      , ppCurrent = xmobarColor "#ABABAB" "" . wrap "[" "]"
+                                                      , ppTitle = xmobarColor "darkgreen" "" . shorten 100
+                                                      }
+                  } 
+
+--}
+
+
+
+
+
+
+
+
+
+-- $usage
+-- To use this module, start with the following @~\/.xmonad\/xmonad.hs@:
+--
+-- > import XMonad
+-- > import XMonad.Config.Gnome
+-- >
+-- > main = xmonad gnomeConfig
+--
+-- For examples of how to further customize @gnomeConfig@ see "XMonad.Config.Desktop".
+
+
+-- | Launch the "Run Application" dialog.  gnome-panel must be running for this
+-- to work.
+gnomeRun :: X ()
+gnomeRun = withDisplay $ \dpy -> do
+    rw <- asks theRoot
+    gnome_panel <- getAtom "_GNOME_PANEL_ACTION"
+    panel_run   <- getAtom "_GNOME_PANEL_ACTION_RUN_DIALOG"
+
+    io $ allocaXEvent $ \e -> do
+        setEventType e clientMessage
+        setClientMessageEvent e rw gnome_panel 32 panel_run 0
+        sendEvent dpy rw False structureNotifyMask e
+        sync dpy False
 
